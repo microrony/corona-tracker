c519a7dbbffa571abc10bac8a24690abff64f26d
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-11-25 17:17:19 +0600

    update

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,26 +1,38 @@
+import { useState } from 'react'
 import GoogleMapReact from 'google-map-react'
 import LocationMarker from './LocationMarker'
 
-const Map = ({ center, zoom }) => {
-  return (
-    <div className='map'>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: 'AIzaSyBZmO84x24ptIrkera2SpjWvpAzWzTePCo' }}
-        defaultCenter={center}
-        defaultZoom={zoom}
-      >
-        <LocationMarker lat={center.lat} lng={center.lng} />
-      </GoogleMapReact>
-    </div>
-  )
+// define constants
+const NATURAL_EVENT_WILDFIRE = 8;
+
+const Map = ({ eventData, center, zoom }) => {
+
+    const markers = eventData.map((ev, index) => {
+        if(ev.categories[0].id === NATURAL_EVENT_WILDFIRE) {
+            return <LocationMarker key={index} lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]} />
+        }
+        return null
+    })
+
+    return (
+        <div className="map">
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: '' }}
+                defaultCenter={ center }
+                defaultZoom={ zoom }
+            >
+                {markers}
+            </GoogleMapReact>
+        </div>
+    )
 }
 
 Map.defaultProps = {
-  center: {
-    lat: 42.3265,
-    lng: -122.8756,
-  },
-  zoom: 6
+    center: {
+        lat: 42.3265,
+        lng: -122.8756
+    },
+    zoom: 6
 }
 
 export default Map
