a0efce361bbabedb3dadfd33df7df21a522268a8
Mahabubur Rahman <microronyy@gmail.com>
2020-12-11 14:36:19 +0600

    created a marker on map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,14 +1,19 @@
-import { MapContainer, TileLayer } from 'react-leaflet'
+import { MapContainer, TileLayer, CircleMarker, Popup } from 'react-leaflet'
 
 const Map = () => {
-  const center = [24.63, 90.45]
+  const position = [43.32, -122.87]
   const zoom = 6
+  const redOptions = { color: 'red' }
+
   return (
-    <MapContainer className="map" center={center} zoom={zoom}>
+    <MapContainer className="map" center={position} zoom={zoom}>
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      <CircleMarker center={position} pathOptions={redOptions} radius={20}>
+      <Popup>Popup in CircleMarker</Popup>
+    </CircleMarker>
     </MapContainer>
   )
 }
