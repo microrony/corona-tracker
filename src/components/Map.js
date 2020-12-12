e38f4301d51a1a9ec595a9d3edac61ebf22ba8f9
Mahabubur Rahman <microronyy@gmail.com>
2020-12-12 17:06:00 +0600

    apply markers using nasa api

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,25 @@
 import { MapContainer, TileLayer, CircleMarker, Popup } from 'react-leaflet'
 
-const Map = () => {
+const Map = ({ eventData }) => {
+  const redOptions = { color: 'red' }
+  const markers = eventData.map((ev, index) => {
+    if (ev.categories[0].id === 8) {
+      return (
+        <CircleMarker
+          key={index}
+          center={[ev.geometries[0].coordinates[1], ev.geometries[0].coordinates[0]]}
+          pathOptions={redOptions}
+          radius={20}
+        >
+          <Popup>Popup in CircleMarker</Popup>
+        </CircleMarker>
+      )
+    }
+    return null
+  })
+
   const position = [43.32, -122.87]
   const zoom = 6
-  const redOptions = { color: 'red' }
 
   return (
     <MapContainer className="map" center={position} zoom={zoom}>
@@ -11,9 +27,7 @@ const Map = () => {
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <CircleMarker center={position} pathOptions={redOptions} radius={20}>
-      <Popup>Popup in CircleMarker</Popup>
-    </CircleMarker>
+      {markers}
     </MapContainer>
   )
 }
